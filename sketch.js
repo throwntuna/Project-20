ee3b5014b19a20d64a4da3497de1a7bd58ca6864
Ancient789 <83103802+Ancient789@users.noreply.github.com>
2021-06-26 20:35:58 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+function preload() {
+    //load the images here   
+     bg = loadImage("images/garden.png");
+    tomImg1= loadAnimation("images/cat1.png");
+    tomImg2=loadAnimation("images/cat2.png", "images/cat3.png");
+    tomImg3= loadAnimation("images/cat4.png");
+    jerryImg1=loadAnimation("images/mouse1.png");
+    jerryImg2= loadAnimation("images/mouse2.png", "images/mouse3.png");
+    jerryImg3=loadAnimation("images/mouse4.png");
+
+}
+
+function setup(){
+    createCanvas(1000, 800);
+    //create tom and jerry sprites here
+   tom = createSprite(870, 600)
+   tom.addAnimation("Tom Animation", tomImg1)
+   tom.scale = 0.2
+   jerry = createSprite(200, 600)
+   jerry.addAnimation("Jery Animation", jerryImg1)
+   jerry.scale = 0.15
+}
+
+function draw() {
+
+    background(bg);
+    //Write condition here to evalute if tom and jerry collide
+
+if (tom.x-jerry.x < (tom.width-jerry.width)/2) {
+    tom.velocityX = 0
+    tom.addAnimation("Tom Colliding", tomImg3)
+    tom.x = 300
+    tom.scale = 0.2
+    jerry.addAnimation("Jerry Colliding", jerryImg3)
+    jerry.scale = 0.15
+    jerry.changeAnimation("Jerry Colliding")
+}
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+ if(keyCode === LEFT_ARROW) {
+     tom.velocityX = -5
+     tom.addAnimation("Tom Running", tomImg2)
+     tom.changeAnimation("Tom Running", tomImg2)
+     jerry.addAnimation("Jerry Running", jerryImg2)
+     jerry.frameDelay = 25
+     jerry.changeAnimation("Jerry Running")
+ }
+
+}
